446238a0ee233a6a3fbae930362340b06af652bd
Dipan Das <dipanisboss@gmail.com>
2025-08-30 03:18:38 +0530

    completed parsing of sheets

--- src/services/parserService.ts
+++ src/services/parserService.ts
@@ -1,6 +1,7 @@
 import type { MultipartFile } from "@fastify/multipart";
 import type { IMetadata, ISearchAPIReq } from "../dto/api.dto.ts";
 import xlsx from "xlsx";
+import type { ISheetData } from "../dto/llm.dto.ts";
 
 export default class FileParser {
     private file: Buffer;
@@ -14,15 +15,35 @@ export default class FileParser {
     async parseSheet() {
         if (this.metadata.mimetype !== 'xlsx') {
             console.error("It is not a spreadsheet, terminating parsing");
-             throw new Error(`Invalid file type, parseSheet() was invoked on ${this.metadata.mimetype} file`);
+            throw new Error(`Invalid file type, parseSheet() was invoked on ${this.metadata.mimetype} file`);
         }
 
-        const workbook = xlsx.read(this.file);
+        const workbook = xlsx.read(this.file, { cellFormula: true });
 
         const sheetNames = workbook.SheetNames;
-        const worksheet = workbook.Sheets[sheetNames[0]];
-        const sheetsRawJson = xlsx.utils.sheet_to_json(worksheet);
 
-        return worksheet;
+        const sheetsRawJson = sheetNames.map(sheetName => {
+            const worksheet = workbook.Sheets[sheetName];
+
+            const data: ISheetData[] = [];
+            for (const cellIndex in worksheet) {
+                const cell = worksheet[cellIndex];
+                const value = cell.v as string;
+                const formula = cell.f as string;
+
+                const sheetData = { cell: cellIndex, value, formula, context: '' };
+                
+                data.push(sheetData);
+            }
+            
+            return {
+                sheetName,
+                summary: '',
+                headers: [''],
+                data,
+            }
+        });
+
+        return sheetsRawJson;
     }
 }
